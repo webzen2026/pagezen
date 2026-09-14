1a55b0942f16069ab5e31bcd1a942ad9331e444f
webzen2026 <323976257+webzen2026@users.noreply.github.com>
2026-09-14 18:25:36 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
 // serving the old cached version.
 // ==========================================
 
-const CACHE_NAME = 'pagezen-v9';
+const CACHE_NAME = 'pagezen-v10';
 
 const ASSETS_TO_CACHE = [
   './',
